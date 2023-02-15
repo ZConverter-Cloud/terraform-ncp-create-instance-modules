bffcd6686072aee0e4fa65775c6a1677c8dcbc0e
ZConverter-samples <85214181+ZConverter-samples@users.noreply.github.com>
2023-02-15 14:39:33 +0900

    Update data_resources.tf

--- data_resources.tf
+++ data_resources.tf
@@ -2,7 +2,12 @@ data "ncloud_zones" "zones" {
   region = var.region
 }
 
-data "ncloud_server_images" "server_image" {}
+data "ncloud_server_images" "server_image" {
+  filter {
+    name = "product_name"
+    values = ["${local.OS_product["${var.support_vpc == false ? "classic" : "vpc"}"][var.OS_name][var.OS_version]}"]
+  }
+}
 
 data "ncloud_server_products" "product" {
   server_image_product_code = local.server_image[0].id
